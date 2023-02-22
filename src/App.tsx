5c0420643f16787b0bb0791dc4e66ebd3b49c2c4
amanjaiswal-cedcoss <amanjaiswal@cedcoss.com>
2023-02-22 09:32:00 +0530

    changes made

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import './App.css';
-import BlogApp from './components/BlogApp';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Blog from "./components/Blog";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <BlogApp/>
+    <div className="container">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/blog" element={<Blog/>}/>
+      </Routes>
+      <Footer/>
     </div>
   );
 }
